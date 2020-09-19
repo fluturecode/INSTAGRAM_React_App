98252778494f3bd469d14d288e073c85b28e6f25
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-18 20:28:03 -0400

    added authentication access

--- src/firebase.js
+++ src/firebase.js
@@ -14,3 +14,5 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
+
+export { db, auth, storage };
