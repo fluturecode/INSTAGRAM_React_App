a76cfc647548d45681f6168973e2416e7bbd32cf
Elizabeth Eidelson <fluturecode@gmail.com>
2020-09-21 14:06:57 -0400

    added user log in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./components/Post";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
-import { Button } from "@material-ui/core";
+import { Button, Input } from "@material-ui/core";
 
 function getModalStyle() {
-	const top = 50 + rand();
-	const left = 50 + rand();
+	const top = 50;
+	const left = 50;
 
 	return {
 		top: `${top}%`,
@@ -30,10 +30,36 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
 	const classes = useStyles();
-	const [modaStyle] = useState(getModalStyle);
-
+	const [modalStyle] = useState(getModalStyle);
 	const [posts, setPosts] = useState([]);
 	const [open, setOpen] = useState(false);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				// user has logged in...
+				console.log(authUser);
+				// this keeps user logged in
+				setUser(authUser);
+
+				if (authUser.displayName) {
+					// don't update username
+				} else {
+					// if just created a new user
+					return authUser.updateProfile({
+						displayName: username,
+					});
+				}
+			} else {
+				// user has logged out....
+				setUser(null);
+			}
+		});
+	}, [user, username]);
 
 	useEffect(() => {
 		// snapshot is listenting for changes to 'posts'
@@ -47,20 +73,50 @@ function App() {
 		});
 	}, []);
 
+	const signUp = (event) => {
+		event.preventDefault();
+
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.catch((err) => alert(err.message));
+	};
+
 	return (
 		<div className="app">
-			<Modal open={open} onClose={() => SetOpen(false)}>
+			<Modal open={open} onClose={() => setOpen(false)}>
 				<div style={modalStyle} className={classes.paper}>
-					<h2>I am a modal</h2>
+					<Form>
+						<img
+							className="app__headerImage"
+							src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+							alt="instagram logo"
+						/>
+						<Input
+							placeholder="username"
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.targe.value)}
+						/>
+						<Input
+							placeholder="email"
+							type="text"
+							value={email}
+							onChange={(e) => setEmail(e.targe.value)}
+						/>
+						<Input
+							placeholder="password"
+							type="passsword"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<Button type="submit" onClick={signUp}>
+							Sign Up
+						</Button>
+					</Form>
 				</div>
 			</Modal>
-			<div className="app__header">
-				<img
-					className="app__headerImage"
-					src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-					alt="instagram logo"
-				/>
-			</div>
+
+			<div className="app__header"></div>
 
 			{posts.map(({ id, post }) => (
 				<Post
